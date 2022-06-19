2211dcae9e2fd68666daea63b7914f913f643b9c
niklaskorz <niklaskorz@users.noreply.github.com>
2022-06-19 10:59:58 +0000

    deploy: 06a4f8777aa69f72e7f41a85eecc5b4a2723ab36

---
+++ workers/recorder.worklet.js
@@ -0,0 +1,78 @@
+(() => {
+  var __defProp = Object.defineProperty;
+  var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+  var __publicField = (obj, key, value) => {
+    __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
+    return value;
+  };
+
+  // workers/recorder.worklet.ts
+  function assert(cond) {
+    if (!cond) {
+      throw new Error("assertion failed");
+    }
+  }
+  var RecorderProcessor = class extends AudioWorkletProcessor {
+    constructor() {
+      super();
+      __publicField(this, "dataPort");
+      __publicField(this, "data");
+      __publicField(this, "bufferSize", 0);
+      __publicField(this, "offset", 0);
+      __publicField(this, "scaleFactor", 1);
+      this.port.onmessage = (e) => {
+        const msg = e.data;
+        switch (msg.type) {
+          case "start":
+            console.log("Starting to record");
+            this.dataPort = e.ports[0];
+            this.bufferSize = msg.bufferSize;
+            this.offset = 0;
+            this.scaleFactor = Math.pow(2, msg.bitsPerSample) / 2;
+            break;
+          case "stop":
+            console.log("Stopping to record");
+            assert(this.dataPort);
+            assert(this.data);
+            this.dataPort.postMessage({
+              type: "end",
+              data: this.data.map((array) => array.slice(0, this.offset))
+            }, this.data.map((array) => array.buffer));
+            this.dataPort = void 0;
+            this.data = void 0;
+            this.port.postMessage({ type: "done" });
+            break;
+        }
+      };
+    }
+    process(inputs, outputs, parameters) {
+      if (!this.dataPort) {
+        return true;
+      }
+      let input = inputs[0];
+      let channels = input.length;
+      let samples = input[0].length;
+      if (!this.data) {
+        this.data = Array.from({ length: channels }, () => new Int32Array(this.bufferSize));
+      }
+      let i = 0;
+      while (i < samples) {
+        for (; i < samples && this.offset + i < this.bufferSize; i++) {
+          for (let channel = 0; channel < channels; channel++) {
+            this.data[channel][this.offset + i] = input[channel][i] * (this.scaleFactor - (input[channel][i] < 0 ? 0 : 1));
+          }
+        }
+        if (this.offset + i >= this.bufferSize) {
+          this.dataPort.postMessage({
+            type: "data",
+            data: this.data
+          });
+          this.offset -= this.bufferSize;
+        }
+      }
+      this.offset += i;
+      return true;
+    }
+  };
+  registerProcessor("recorder", RecorderProcessor);
+})();
